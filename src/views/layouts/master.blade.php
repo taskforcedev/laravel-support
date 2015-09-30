97da82ebf08974c571b5210969d5e982cae51344
PrivateSniper <privatesniper@gmail.com>
2015-09-30 11:22:05 +0100

    Add username into navbar if user is auth

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -20,6 +20,11 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+        @if (\Auth::check())
+            <li><a>{{ \Auth::user()->name }}</a></li>
+        @endif
+        </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
