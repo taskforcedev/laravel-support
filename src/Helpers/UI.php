fc7e8bc29a13ced08db60158763b946ef09cc4b9
PrivateSniper <privatesniper@gmail.com>
2016-12-23 09:00:59 +0000

    Add framework column generator.

--- src/Helpers/UI.php
+++ src/Helpers/UI.php
@@ -17,20 +17,90 @@ class UI
 
     public function cssClass($type)
     {
-        if ($this->bs3) {
-            $framework = $this->json->bs3;
-        } elseif ($this->bs4) {
-            $framework = $this->json->bs4;
-        } elseif ($this->f6) {
-            $framework = $this->json->f6;
-        } else {
-            return '';
-        }
+        $framework = $this->getFramework();
+        $classes = $this->getClasses($framework);
 
-        if (property_exists($framework, $type)) {
-            return $framework->$type;
+        if (property_exists($classes, $type)) {
+            return $classes->$type;
         }
 
         return '';
+    }
+
+    /**
+     * Create framework column class.
+     *
+     * @param integer $xs Extra Small (Ignored on Foundation
+     * @param integer $sm Small.
+     * @param integer $md Medium.
+     * @param integer $lg Large.
+     *
+     * @return string
+     */
+    public function columns($xs, $sm, $md, $lg)
+    {
+        $framework = $this->getFramework();
+        switch ($framework)
+        {
+            case 'bs3':
+            case 'bs4':
+                $xsC = $this->cssClass('column_extra_small') . $xs;
+                $smC = $this->cssClass('column_small') . $sm;
+                $mdC = $this->cssClass('column_medium') . $md;
+                $lgC = $this->cssClass('column_large') . $lg;
+                return $this->buildClassString([$xsC, $smC, $mdC, $lgC]);
+            case 'f6':
+                // Build the foundation classes
+                $smC = $this->cssClass('column_small') . $sm;
+                $mdC = $this->cssClass('column_medium') . $md;
+                $lgC = $this->cssClass('column_large') . $lg;
+                $columns = $this->cssClass('column');
+                // Build the string
+                return $this->buildClassString([$columns, $smC, $mdC, $lgC]);
+            default:
+                return '';
+                break;
+        }
+    }
+
+    /**
+     * Get the UI framework name.
+     * @return string
+     */
+    public function getFramework()
+    {
+        if ($this->bs3) {
+            return 'bs3';
+        } elseif ($this->bs4) {
+            return 'bs4';
+        } elseif ($this->f6) {
+            return 'f6';
+        }
+        return '';
+    }
+
+    public function getClasses($framework)
+    {
+        switch ($framework)
+        {
+            case 'bs3':
+                $classes = $this->json->bs3;
+                break;
+            case 'bs4':
+                $classes = $this->json->bs4;
+                break;
+            case 'f6':
+                $classes = $this->json->f6;
+                break;
+            case '':
+            default:
+                return '';
+        }
+        return $classes;
+    }
+
+    public function buildClassString($classes = [])
+    {
+        return join(' ', $classes);
     }
 }
