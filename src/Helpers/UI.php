1821ead7bea3f290bce9fed00e64e368d23df826
PrivateSniper <privatesniper@gmail.com>
2016-12-23 08:14:57 +0000

    Add ui framework classes json file.

--- src/Helpers/UI.php
+++ src/Helpers/UI.php
@@ -5,25 +5,32 @@ class UI
     public $bs3;
     public $bs4;
     public $f6;
+    public $json;
 
     public function __construct()
     {
         $this->bs3 = config('taskforce-support.frontend.libraries.bootstrap3');
-        $this->bs4 = config('taskforce-support.frontend.libraries.bootstrap3');
+        $this->bs4 = config('taskforce-support.frontend.libraries.bootstrap4');
         $this->f6  = config('taskforce-support.frontend.libraries.foundation6');
+        $this->json = json_decode(file_get_contents(__DIR__ . '/UI/classes.json'));
     }
 
-    public function getInputClasses()
+    public function getClasses($type)
     {
-        $classes = '';
-
-        if ($this->bs3 || $this->bs4) {
-            // Bootstrap 3/4
-            $classes = 'form-control';
+        if ($this->bs3) {
+            $framework = $this->json->bs3;
+        } elseif ($this->bs4) {
+            $framework = $this->json->bs4;
         } elseif ($this->f6) {
-            // Foundation 6
+            $framework = $this->json->f6;
+        } else {
+            return '';
         }
 
-        return $classes;
+        if (property_exists($framework, $type)) {
+            return $framework->$type;
+        }
+
+        return '';
     }
 }
