f7df29a18016d9c39033e833157c00d31d54aea8
PrivateSniper <privatesniper@gmail.com>
2016-12-28 15:20:02 +0000

    Add additional yields:
    * head
    * styles
    * navigation

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -3,6 +3,8 @@
 <head>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    @yield('head')
+    @yield('styles')
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -24,6 +26,7 @@
         @if (\Auth::check())
             <li><a>{{ \Auth::user()->name }}</a></li>
         @endif
+        @yield('navigation')
         </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
