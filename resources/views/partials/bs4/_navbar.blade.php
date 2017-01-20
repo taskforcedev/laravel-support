5d195577a5c7fe4ca7c59ed234dd21642b776403
PrivateSniper <privatesniper@gmail.com>
2017-01-20 11:13:17 +0000

    Update bs4 navbar (testing).

--- resources/views/partials/bs4/_navbar.blade.php
+++ resources/views/partials/bs4/_navbar.blade.php
@@ -1,25 +1,14 @@
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">{{ $sitename or "Laravel" }}</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                @if (\Auth::check())
-                    <li><a>{{ \Auth::user()->name }}</a></li>
-                @endif
-                @yield('navigation')
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">{{ $sitename or "Laravel" }}</a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            @if (\Auth::check())
+                <li><a>{{ \Auth::user()->name }}</a></li>
+            @endif
+            @yield('navigation')
+        </ul>
+    </div>
 </nav>
