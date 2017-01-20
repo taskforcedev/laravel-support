ea85234ec5a644dfe6bbac3fa728f3af49c2c9a4
PrivateSniper <privatesniper@gmail.com>
2017-01-20 09:41:14 +0000

    Split bootstrap includes into seperate partials.

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -6,8 +6,7 @@ $partial = 'taskforce-support::partials.' . $framework;
 ?><!DOCTYPE html>
 <html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    @include($partial . '._head')
     @yield('head')
     @yield('styles')
 </head>
@@ -20,9 +19,7 @@ $partial = 'taskforce-support::partials.' . $framework;
     @yield('breadcrumbs')
     @yield('content')
 </div>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    @include($partial . '._scripts')
     @yield('scripts')
 </body>
 </html>
---
+++ src/views/partials/f6/_scripts.blade.php
@@ -0,0 +1,1 @@
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
