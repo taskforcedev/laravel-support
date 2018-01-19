a8af4962b43bca21922d58b6d164516ec36ab261
davidcraig <d.craig@redu.co.uk>
2018-01-19 11:46:41 +0000

    Update bootstrap 4 css/js to 4.0.0 release.

--- resources/views/partials/bs4/_head.blade.php
+++ resources/views/partials/bs4/_head.blade.php
@@ -1,1 +1,1 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
