e336d2837a916334521f8edd76fda8c691b904ce
davidcraig <d.craig@redu.co.uk>
2018-02-06 15:06:44 +0000

    Fix bootstrap 4 navbar.

--- resources/views/partials/bs4/_navbar.blade.php
+++ resources/views/partials/bs4/_navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">{{ $sitename or "Laravel" }}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">{{ $sitename or "Laravel" }}</a>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
             @if (\Auth::check())
